f71ae42c065ad4b703aaac167a3fd1bd06ccaf02
catanswer <564029918@qq.com>
2021-04-30 10:51:19 +0800

    后端生成静态资源到目录 供前台访问

--- express/controller/index.js
+++ express/controller/index.js
@@ -1,3 +1,9 @@
+const path = require('path')
+const template = require('art-template')
+const fs = require('fs')
+
+const { data } = require('../model/list')
+
 const getList = (req, res, next) => {
   // let data = '<ul>'
   // for (let i = 0; i < 100; i++) {
@@ -6,18 +12,24 @@ const getList = (req, res, next) => {
   // data += '</ul>'
   // res.send(data)
 
-  let dataArray = []
-  for (let i = 0; i < 100; i++) {
-    dataArray.push('line' + i)
-  }
+  
   // res.set({ 'content-type': 'application/json;charset=utf-8'})
   // 使用模板
   // res.render('list.art', {
   //   data: JSON.stringify(dataArray)
   // })
-  res.render('list-html.art', {
-    data: dataArray
+
+  // res.render('list-html.art', {
+  //   data: dataArray
+  // })
+
+  // 读取模板文件
+  const html = template(path.resolve(__dirname, '../views/list-html.art'), {
+    data
   })
+  // 写入到静态资源目录中
+  fs.writeFileSync(path.join(__dirname, '../public/list.html'), html)
+  res.send()
 }
 
 module.exports = {
